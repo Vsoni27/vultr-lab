2b7c3c7b2b627f0ceeb2978fe914733953c9a11e
Vsoni27 <sonisheela1977@gmail.com>
2024-11-10 11:51:58 +0530

    added test User

--- app/courses/[id]/page.tsx
+++ app/courses/[id]/page.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Progress } from "@/components/ui/progress";
+import Profile from "@/components/Profile";
 
 interface Step {
   title: string;
@@ -15,8 +16,17 @@ interface ParamsProps {
   params: { id: string };
 }
 
+interface TestUserType {
+  message: string;
+  email: string;
+  name: string;
+  password: string;
+  id: string;
+}
+
 const CourseDetails = ({ params }: { params: Promise<{ id: string }> }) => {
   const [steps, setSteps] = useState<Step[]>([]);
+  // const [testUser, setTestUser] = useState<TestUserType>();
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -26,9 +36,15 @@ const CourseDetails = ({ params }: { params: Promise<{ id: string }> }) => {
         setLoading(true);
         console.log(id);
 
+        // const userResponse = await axios.get(`/api/sub-account?id=${id}`);
+        // setTestUser({
+        //   ...userResponse.data,
+        // });
+        // console.log(userResponse.data);
         const response = await axios.get(`/api/lab`, {
           params: {
             id: id,
+            // userId: userResponse.data.id,
           },
         });
         console.log(response.data);
@@ -53,42 +69,47 @@ const CourseDetails = ({ params }: { params: Promise<{ id: string }> }) => {
     );
   } else
     return (
-      <div className="p-8 bg-slate-900 min-h-screen">
-        <h1 className="text-white text-2xl mb-4">Steps</h1>
-        {steps.map((item: Step) => (
-          <div
-            key={item.title}
-            className={cn(
-              "p-4 border rounded-md my-4 flex items-center",
-              item.isDone
-                ? "bg-green-500 text-white"
-                : "bg-slate-800 text-white"
-            )}
-          >
-            <input
-              type="checkbox"
-              checked={item.isDone}
-              onChange={async () => {
-                try {
-                  item.isDone = !item.isDone;
-                  setSteps([...steps]);
+      <div className="p-8 bg-slate-900 min-h-screen flex flex-row space-x-8">
+        <div>
+          <h1 className="text-white text-2xl mb-4">Steps</h1>
+          {steps.map((item: Step) => (
+            <div
+              key={item.title}
+              className={cn(
+                "p-4 border rounded-md my-4 flex items-center",
+                item.isDone
+                  ? "bg-green-500 text-white"
+                  : "bg-slate-800 text-white"
+              )}
+            >
+              <input
+                type="checkbox"
+                checked={item.isDone}
+                onChange={async () => {
+                  try {
+                    item.isDone = !item.isDone;
+                    setSteps([...steps]);
 
-                  const updateSteps = await axios.put(`/api/lab`, {
-                    id: Number((await params).id),
-                    steps: [...steps],
-                  });
-                } catch (error) {
-                  console.log(error);
-                }
-              }}
-              className="mr-4 p-16"
-            />
-            <div>
-              <h2 className="text-lg font-semibold">{item.title}</h2>
-              <p className="text-sm mt-2">{item.description}</p>
+                    const updateSteps = await axios.put(`/api/lab`, {
+                      id: Number((await params).id),
+                      steps: [...steps],
+                    });
+                  } catch (error) {
+                    console.log(error);
+                  }
+                }}
+                className="mr-4 p-16"
+              />
+              <div>
+                <h2 className="text-lg font-semibold">{item.title}</h2>
+                <p className="text-sm mt-2">{item.description}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+        <div className="-translate-y-40">
+          <Profile/>
+        </div>
       </div>
     );
 };
--- components/Profile.tsx
+++ components/Profile.tsx
@@ -5,28 +5,30 @@ import { Button } from "@/components/ui/button";
 import { UserDetailsType } from "@/lib/types";
 import Image from "next/image";
 
-export default function Profile({ user }: { user: UserDetailsType }) {
+interface TestUserType {
+  message: string;
+  email: string;
+  name: string;
+  password: string;
+  id: string;
+}
+
+export default function Profile() {
   return (
     <div className="flex justify-center items-center min-h-screen">
       <Card className="w-full max-w-md p-4 shadow-lg bg-blue-50 rounded-lg border border-blue-200">
         <CardHeader className="flex flex-col items-center">
-          <Image
-            src={user.picture}
-            alt={`${user.username}'s profile`}
-            width={40}
-            height={40}
-            className="w-24 h-24 rounded-full border-4 border-blue-400"
-          />
           <CardTitle className="mt-4 text-2xl font-bold text-blue-700">
-            {user.username}
+            Test User 1645
           </CardTitle>
         </CardHeader>
         <CardContent className="text-center">
-          <p className="text-blue-600 text-md  mb-2">Email</p>
-          <p className="text-gray-700 text-lg">{user.email}</p>
-          <Button className="mt-6 w-full bg-blue-500 text-white hover:bg-blue-600">
-            Edit Profile
-          </Button>
+          <p className="text-blue-600 text-md  mb-2 font-bold">Email</p>
+          <p className="text-gray-700 text-lg font-bold">
+            vultr1645.void356@simplelogin.com
+          </p>
+          <p className="text-blue-600 text-md  mb-2 font-bold">Password</p>
+          <p className="text-gray-700 text-lg font-bold">testpassword1645</p>
         </CardContent>
       </Card>
     </div>
